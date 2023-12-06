fd656179a69cb90c257b3587c2628b8d95fe6f07
Jesus Soledad <jesusrflapp@gmail.com>
2023-12-06 12:28:50 -0300

    fix: (index.js) path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (!day) {
   throw 'Day param required';
 }
 
-const path = `./2023/day/${day}/`;
+const path = `./2023/day/${day}`;
 
 import(`${path}/index.js`).then(module => {
   console.log(module.execute(readFileSync(`${path}/input.txt`, 'utf-8').split(/\r?\n/)));
